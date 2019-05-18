491c73a6b75d98bf717ed194d8722186fee5b24c
Richard Silva <richardfsilva@protonmail.com>
2019-05-18 16:06:06 -0300

    Codigo Aula 1

---
+++ Aula1/app.py
@@ -0,0 +1,26 @@
+"""
+print("Olá "+"Mundo")
+
+nome = "Manoel"
+sobrenome = "Santos"
+
+#print(nome+" "+sobrenome)
+
+print("Olá meu nome é {} e meu sobrenome é {}.".format(nome, sobrenome))
+print("Olá meu nome é {0} e meu sobrenome é {1}".format(nome, sobrenome))
+
+# Primeira variavel
+nome2 = "John"
+# variavel String
+endereco = "Rua 12" 
+
+print(f"Meu nome é {nome2} e moro na {endereco}.")
+"""
+# asdsadsad
+
+nome = "MANOEL SANTOS"
+
+# print(nome.upper())
+# print(nome.lower())
+
+print(nome.replace("SANTOS", "SIlVA"))
