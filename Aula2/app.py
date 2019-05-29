da38409a208b5a3599eceb1b11e368682cfa2182
Richard Silva <richardfsilva@protonmail.com>
2019-05-29 18:48:32 -0300

    Codigo Aula 2

---
+++ Aula2/app.py
@@ -0,0 +1,23 @@
+# Numeros inteiros (int)
+num = 20
+num2 = 30
+
+# Numeros decimais (float)
+num3 = 5.23
+num4 = 3.14
+
+resultado = num3 * num4
+
+# Mostrar 2 casa decimais
+print(f"Resultado: {resultado:.2f}")
+# ou
+print("Resultado: {:.2f}".format(resultado))
+
+'''
+Operacoes Matematicas
+
+Soma (+)	Subtracao (-)	Multiplicacao (*)	Divisao (/)
+Divisao Inteira (//)	Resto (%)	Potencia (**)
+
+'''
+
